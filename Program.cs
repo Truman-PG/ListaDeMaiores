03ece8ed5ec6871b867e662ddd0b8ed6c1ddfa52
Aluno <Aluno@DESKTOP-DMGHJ3C>
2024-04-27 12:15:49 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Quantas pessoas irá ter?");
+int[] idades = new int[int.Parse(Console.ReadLine())];
+
+string[] nomes = new string[idades.Length];
+
+int maiores = 0;
+
+for (int i = 0; i < idades.Length; i++)
+{
+    Console.WriteLine($"Qual a {i + 1} idade?");
+    idades[i] = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Qual o nome?");
+    nomes[i] = Console.ReadLine();
+
+    if (idades[i] >= 18)
+    {
+        maiores++;
+    }
+}
+Console.WriteLine($"{maiores} pessoas são maiores de idade");
